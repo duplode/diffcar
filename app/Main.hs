0badb82c1d374c558c1664c0e0ef7d64be4c4277
Daniel Mlot <duplode_1@yahoo.com.br>
2023-03-19 19:52:54 -0300

    Option to not generate output for equivalent files

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,22 @@
 module Main where
 
 import Car
+
 import qualified Options.Applicative as Opts
 import qualified Data.Text as T
 import qualified Data.Text.IO as T (putStrLn)
-import Control.Monad (when)
+import qualified Data.Text.Lazy as TL (toStrict)
+import Control.Monad (when, unless)
+import Data.Maybe (isNothing)
+import qualified Data.Portray.Diff as Portray
+import qualified Data.Portray.Prettyprinter as Portray
 
 data Options = Options
     { file1 :: FilePath
     , file2 :: FilePath
     , plain :: Bool
     , header :: Bool
+    , skipEquals :: Bool
     }
 
 argFile1 :: Opts.Parser FilePath
@@ -34,10 +40,15 @@ switchHeader = Opts.switch $
     Opts.long "header"
     <> Opts.help "Print header with file names"
 
+switchSkipEquals :: Opts.Parser Bool
+switchSkipEquals = Opts.switch $
+    Opts.long "skip-equals"
+    <> Opts.help "Don't generate output for equal files"
+
 baseOpts :: Opts.Parser Options
 baseOpts = Options
     <$> argFile1 <*> argFile2
-    <*> switchPlain <*> switchHeader
+    <*> switchPlain <*> switchHeader <*> switchSkipEquals
 
 opts :: Opts.ParserInfo Options
 opts = Opts.info baseOpts $
@@ -49,13 +60,19 @@ main = do
     options <- Opts.execParser opts
     let p1 = file1 options
         p2 = file2 options
-    when (header options) $ T.putStrLn (headerText p1 p2)
-    ppdCarRes (plain options) p1 p2
-    when (header options) $ T.putStrLn ""
-    where
-    headerText p1 p2 = T.unlines
-        [ T.replicate 72 "-"
-        , T.pack p1
-        , T.pack p2
-        , T.replicate 72 "-"
-        ]
+    car1 <- readCar p1
+    car2 <- readCar p2
+    let diffCar = Portray.diff car1 car2
+        headerText  = T.unlines
+            [ T.replicate 72 "-"
+            , T.pack p1
+            , T.pack p2
+            , T.replicate 72 "-"
+            ]
+        chosenShow = if plain options
+            then Portray.basicShowPortrayal
+            else TL.toStrict . Portray.prettyShowPortrayalLazy
+    unless (skipEquals options && isNothing diffCar) $ do
+        when (header options) $ T.putStrLn headerText
+        T.putStrLn (maybe "_" chosenShow diffCar)
+        when (header options) $ T.putStrLn ""
